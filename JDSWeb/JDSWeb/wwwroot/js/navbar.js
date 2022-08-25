29f4f4ee7c62ad50a6f27a92ef4fe7ae21968a15
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-08-25 11:25:43 +0200

    Fix rebase navbar on dev (2/2)

---
+++ JDSWeb/JDSWeb/wwwroot/js/navbar.js
@@ -0,0 +1,28 @@
+﻿
+/**
+ * Navbar mobile toggle when :
+ * - Click on burger
+ * - Click next to mobile menu
+ * - Resize page over 990px
+ */
+
+$(document).ready(() => {
+    $("#jds-burger").click(() => {
+        toggleMobileMenu();
+    });
+
+    $("#jds-close-nav").click(() => {
+        toggleMobileMenu();
+    });
+
+    $(window).resize(() => {
+        if ($("#jds-mobile-nav").hasClass("jds-mobile-nav-opened") && $(document).width() >= 990) {
+            toggleMobileMenu();
+        }
+    });
+
+    function toggleMobileMenu() {
+        $("#jds-mobile-nav").toggleClass("jds-mobile-nav-opened");
+        $("#jds-close-nav").toggleClass("jds-close-nav-opened");
+    }
+});
