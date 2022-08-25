07b59846887d5b725a841075e47392c5fa316ef0
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-08-25 11:24:23 +0200

    Fix rebase navbar on dev (1/2)

--- JDSWeb/JDSWeb/wwwroot/js/site.js
+++ JDSWeb/JDSWeb/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const menuHamburger = document.querySelector(".jds-burger");
+const navMobileMenu = document.querySelector(".jds-mobile-nav");
+const test = document.querySelector(".jds-close-nav");
+
+menuHamburger.addEventListener("click", () => {
+    navMobileMenu.classList.toggle("jds-mobile-nav-opened");
+    test.classList.toggle("jds-close-nav-opened");
+})
+
+// TODO : Refaire au propre + listener sur redimensionnement page
+//        (étirement, le menu doit disparaître)
