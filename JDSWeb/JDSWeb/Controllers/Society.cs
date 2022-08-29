21e4af7d79947cae124d3af2ef253c6792134195
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-08-29 11:01:04 +0200

    dev(Committee) : Committee animation cards

---
+++ JDSWeb/JDSWeb/Controllers/Society.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JDSWeb.Controllers
+{
+    public class Society : Controller
+    {
+        public IActionResult Committee()
+        {
+            return View();
+        }
+    }
+}
