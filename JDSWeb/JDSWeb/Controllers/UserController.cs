d9e69eda27afbf1367ef3f6716a78c709c4bfde5
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2022-09-01 13:48:32 +0200

    #21 User deletion for Admins

--- JDSWeb/JDSWeb/Controllers/UserController.cs
+++ JDSWeb/JDSWeb/Controllers/UserController.cs
@@ -84,10 +84,10 @@ namespace JDSWeb.Controllers
         // GET: UserController
         public ActionResult List()
         {
-            //if ((ERole)(HttpContext.Session.GetInt32(UserViewModel.SessionKeyUserRole) ?? -1) < ERole.Manager)
-            //{
-            //    return RedirectToAction("Home", "Index");
-            //}
+            if ((ERole)(HttpContext.Session.GetInt32(UserViewModel.SessionKeyUserRole) ?? -1) < ERole.Manager)
+            {
+                return RedirectToAction("Home", "Index");
+            }
 
             JDSContext ctx = new JDSContext();
 
@@ -168,73 +168,31 @@ namespace JDSWeb.Controllers
             return RedirectToAction("List", "User");
         }
 
-        // GET: UserController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: UserController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: UserController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UserController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: UserController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: UserController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
-        }
+            if ((ERole)(HttpContext.Session.GetInt32(UserViewModel.SessionKeyUserRole) ?? -1) < ERole.Manager)
+            {
+                return RedirectToAction("Home", "Index");
+            }
 
-        // POST: UserController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
+            JDSContext ctx = new JDSContext();
+
+            User? user = ctx.Users.Fetch().FirstOrDefault(u => u.Id == id);
+
+            if(user is null)
             {
-                return RedirectToAction(nameof(Index));
+                ctx.Dispose();
+
+                return RedirectToAction("List", "User");
             }
-            catch
-            {
-                return View();
-            }
+
+            ctx.Users.Remove(user);
+
+            ctx.SaveChanges();
+
+            ctx.Dispose();
+
+            return RedirectToAction("List", "User");
         }
     }
 }
